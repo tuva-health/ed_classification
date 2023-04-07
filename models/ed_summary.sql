c666c9a3a738a7177a518b89850932b10be6612a
Lipsa Panda <lpanda2014@gmail.com>
2023-04-07 18:37:19 -0400

    [FIXED] naming convention

--- models/ed_summary.sql
+++ models/ed_summary.sql
@@ -9,10 +9,10 @@ with summary as (
      classification_order
      , classification_name
      --, ccs_description_with_covid
-     , count(*) as condition_row_ct
-     , count(distinct(encounter_id)) as encounter_ct
-     , count(distinct(claim_id)) as claim_ct
-     , count(distinct(patient_id)) as patient_ct
+     , count(*) as condition_row_count
+     , count(distinct(encounter_id)) as encounter_count
+     , count(distinct(claim_id)) as claim_count
+     , count(distinct(patient_id)) as patient_count
      , sum(claim_paid_amount_sum) as claim_paid_amount_sum
    from {{ ref('ed_classification__johnston_conditions_with_class') }}
    inner join {{ ref('ed_classification_categories') }} using(classification)
@@ -21,7 +21,7 @@ with summary as (
 
 select
     summary.*
-    , 100 * ratio_to_report(claim_ct) over() as pct_claim_row_ct
-    , 100 * ratio_to_report(claim_paid_amount_sum) over() as pct_claim_paid_amount_sum
+    , 100 * ratio_to_report(claim_count) over() as percent_claim_row_count
+    , 100 * ratio_to_report(claim_paid_amount_sum) over() as percent_claim_paid_amount_sum
 from summary
 order by classification_order asc
