fc2cdf0172de7272c91dfc0d793cac85f424b287
Lipsa Panda <lpanda2014@gmail.com>
2023-04-11 10:55:48 -0400

    [FIXED] another issue with encounter count not being there

--- models/ed_summary.sql
+++ models/ed_summary.sql
@@ -6,10 +6,9 @@ select
   , ccs_description_with_covid
   , condition_date_year
   , condition_date_year_month
-  , count(distinct(encounter_id)) as encounter_count
   , count(distinct(claim_id)) as claim_count
   , sum(claim_paid_amount_sum) as claim_paid_amount_sum
 
 from {{ ref('ed_classification__johnston_conditions_with_class') }}
 inner join {{ ref('ed_classification_categories') }} using(classification)
-group by 1, 2, 3, 4, 5, 6
+group by 1, 2, 3, 4, 5
