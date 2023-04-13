b651822cf305edff23c6058857d31a4b9ff27d6c
Lipsa Panda <lpanda2014@gmail.com>
2023-04-13 09:47:00 -0400

    [ADDED] updated mart views on the ed summary cube

---
+++ models/mart/by_provider_practice_state.sql
@@ -0,0 +1,22 @@
+with summary as (
+  select
+     classification_order
+     , classification_name
+     , condition_date_year
+     , provider_practice_state
+     , sum(claim_count) as claim_count
+     , sum(claim_paid_amount_sum) as claim_paid_amount_sum
+
+  from {{ ref('ed_summary') }}
+  group by 1,2,3,4
+)
+
+select
+  summary.*
+  , 100 * ratio_to_report(claim_count)
+    over(partition by classification_name) as percent_claim_count_of_classification
+  , 100 * ratio_to_report(claim_paid_amount_sum)
+    over(partition by classification_name) as percent_claim_paid_amount_sum_of_classification
+
+from summary
+order by 6 desc
---
+++ models/mart/patient_list.sql
@@ -0,0 +1,20 @@
+/*
+All Conditions with all available relevant patient information
+*/
+
+select
+   p.*
+   , c.claim_id
+   , c.condition_date
+   , c.condition_date_year
+   , c.condition_date_year_month
+   , c.code_type
+   , c.code
+   , c.description
+   , c.ccs_description_with_covid
+   , ec.classification_order
+   , c.classification
+   , ec.classification_name
+from {{ ref('ed_classified_condition_with_class') }} c
+inner join {{ var('patient') }} p using(patient_id)
+inner join {{ ref('ed_classification_categories') }} ec using(classification)
