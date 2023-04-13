f369509d20331a69bbb9e519143d67affc72701c
Lipsa Panda <lpanda2014@gmail.com>
2023-04-13 10:01:33 -0400

    [FIXED] this doesn't need the ratio to report

--- models/mart/by_year.sql
+++ models/mart/by_year.sql
@@ -1,23 +1,13 @@
 -- {{ config(enabled=var('ed_classification_enabled',var('tuva_packages_enabled',True))) }}
 
-with summary as (
-   select
-      classification_order
-      , classification_name
-      , condition_date_year
-      , sum(claim_count) as claim_count
-      , sum(claim_paid_amount_sum) as claim_paid_amount_sum
-
-   from {{ ref('ed_summary') }}
-   group by 1,2,3
-)
 
 select
-    summary.*
-    , 100 * ratio_to_report(claim_count)
-      over(partition by classification_name) as percent_claim_count_of_classification
-    , 100 * ratio_to_report(claim_paid_amount_sum)
-       over(partition by classification_name) as percent_claim_paid_amount_sum_of_classification
+   classification_order
+   , classification_name
+   , condition_date_year
+   , sum(claim_count) as claim_count
+   , sum(claim_paid_amount_sum) as claim_paid_amount_sum
 
-from summary
-order by 3 desc
+from {{ ref('ed_summary') }}
+group by 1,2,3
+order by 3, 1
